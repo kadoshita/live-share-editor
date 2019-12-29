461cf829778e6dc6fabc66a22a638ba24cbd2f1a
kadoshita <sublimer.me@gmail.com>
2019-12-29 21:54:28 +0900

    add mode theme change

--- ClientApp/src/components/Editor.js
+++ ClientApp/src/components/Editor.js
@@ -3,15 +3,63 @@ import * as SignalR from '@microsoft/signalr';
 import ReactAce from 'react-ace';
 
 import 'ace-builds/src-noconflict/mode-c_cpp';
+import 'ace-builds/src-noconflict/mode-csharp';
+import 'ace-builds/src-noconflict/mode-java';
+import 'ace-builds/src-noconflict/mode-python';
+import 'ace-builds/src-noconflict/mode-php';
+import 'ace-builds/src-noconflict/mode-ruby';
+import 'ace-builds/src-noconflict/mode-golang';
+import 'ace-builds/src-noconflict/mode-html';
+import 'ace-builds/src-noconflict/mode-css';
+import 'ace-builds/src-noconflict/mode-javascript';
+import 'ace-builds/src-noconflict/mode-markdown';
+import 'ace-builds/src-noconflict/mode-plain_text';
+
 import 'ace-builds/src-noconflict/theme-monokai';
+import 'ace-builds/src-noconflict/theme-cobalt';
+import 'ace-builds/src-noconflict/theme-terminal';
+import 'ace-builds/src-noconflict/theme-twilight';
+import 'ace-builds/src-noconflict/theme-chrome';
+import 'ace-builds/src-noconflict/theme-eclipse';
+import 'ace-builds/src-noconflict/theme-github';
+import 'ace-builds/src-noconflict/theme-textmate';
+import 'ace-builds/src-noconflict/theme-xcode';
+import 'ace-builds/src-noconflict/theme-dreamweaver';
 
 export class Editor extends Component {
     static displayName = Editor.name;
-
+    langList = [
+        { name: 'C/C++', value: 'c_cpp' },
+        { name: 'C#', value: 'csharp' },
+        { name: 'Java', value: 'java' },
+        { name: 'Python', value: 'python' },
+        { name: 'PHP', value: 'php' },
+        { name: 'Ruby', value: 'ruby' },
+        { name: 'Go', value: 'golang' },
+        { name: 'HTML', value: 'html' },
+        { name: 'CSS', value: 'css' },
+        { name: 'JavaScript', value: 'javascript' },
+        { name: 'Markdown', value: 'markdown' },
+        { name: 'Plain Text', value: 'plain_text' }
+    ];
+    themeList = [
+        'monokai',
+        'cobalt',
+        'terminal',
+        'twilight',
+        'chrome',
+        'eclipse',
+        'github',
+        'textmate',
+        'xcode',
+        'dreamweaver'
+    ];
     constructor(props) {
         super(props);
         this.state = {
-            sendText: ''
+            mode: 'c_cpp',
+            theme: 'monokai',
+            code: ''
         };
         this.sendTextBinded = this.sendText.bind(this);
         this.connection = new SignalR.HubConnectionBuilder().withUrl("/shareHub").build();
@@ -23,6 +71,7 @@ export class Editor extends Component {
     }
 
     sendText(sendText) {
+        this.setState({ code: sendText });
         this.connection.invoke("SendMessage", sendText).catch(err => {
             console.error(err);
         });
@@ -30,11 +79,25 @@ export class Editor extends Component {
     render() {
         return (
             <div>
+                <label>言語:
+                    <select onChange={e => this.setState({ mode: e.target.value })}>
+                        {this.langList.map(l => <option key={l.value} value={l.value}>{l.name}</option>)}
+                    </select>
+                </label>
+                <label>テーマ:
+                    <select onChange={e => this.setState({ theme: e.target.value })}>
+                        {this.themeList.map(l => <option key={l} value={l}>{l}</option>)}
+                    </select>
+                </label>
                 <ReactAce
                     width='100%'
-                    mode='c_cpp'
-                    theme='monokai'
+                    mode={this.state.mode}
+                    theme={this.state.theme}
                     onChange={v => this.sendTextBinded(v)}
+                    value={this.state.code}
+                    setOptions={{
+                        useWorker: false
+                    }}
                 >
                 </ReactAce>
             </div>
--- ClientApp/src/components/Viewer.js
+++ ClientApp/src/components/Viewer.js
@@ -3,14 +3,49 @@ import * as SignalR from '@microsoft/signalr';
 import ReactAce from 'react-ace';
 
 import 'ace-builds/src-noconflict/mode-c_cpp';
+import 'ace-builds/src-noconflict/mode-csharp';
+import 'ace-builds/src-noconflict/mode-java';
+import 'ace-builds/src-noconflict/mode-python';
+import 'ace-builds/src-noconflict/mode-php';
+import 'ace-builds/src-noconflict/mode-ruby';
+import 'ace-builds/src-noconflict/mode-golang';
+import 'ace-builds/src-noconflict/mode-html';
+import 'ace-builds/src-noconflict/mode-css';
+import 'ace-builds/src-noconflict/mode-javascript';
+import 'ace-builds/src-noconflict/mode-markdown';
+import 'ace-builds/src-noconflict/mode-plain_text';
+
 import 'ace-builds/src-noconflict/theme-monokai';
+import 'ace-builds/src-noconflict/theme-cobalt';
+import 'ace-builds/src-noconflict/theme-terminal';
+import 'ace-builds/src-noconflict/theme-twilight';
+import 'ace-builds/src-noconflict/theme-chrome';
+import 'ace-builds/src-noconflict/theme-eclipse';
+import 'ace-builds/src-noconflict/theme-github';
+import 'ace-builds/src-noconflict/theme-textmate';
+import 'ace-builds/src-noconflict/theme-xcode';
+import 'ace-builds/src-noconflict/theme-dreamweaver';
+
 
 export class Viewer extends Component {
     static displayName = Viewer.name;
-
+    themeList = [
+        'monokai',
+        'cobalt',
+        'terminal',
+        'twilight',
+        'chrome',
+        'eclipse',
+        'github',
+        'textmate',
+        'xcode',
+        'dreamweaver'
+    ];
     constructor(props) {
         super(props);
         this.state = {
+            mode: 'c_cpp',
+            theme: 'monokai',
             receiveText: ''
         };
         this.connection = new SignalR.HubConnectionBuilder().withUrl("/shareHub").build();
@@ -27,11 +62,19 @@ export class Viewer extends Component {
     render() {
         return (
             <div>
+                <label>テーマ:
+                    <select onChange={e => this.setState({ theme: e.target.value })}>
+                        {this.themeList.map(l => <option key={l} value={l}>{l}</option>)}
+                    </select>
+                </label>
                 <ReactAce
                     width='100%'
-                    mode='c_cpp'
-                    theme='monokai'
+                    mode={this.state.mode}
+                    theme={this.state.theme}
                     value={this.state.receiveText}
+                    setOptions={{
+                        useWorker: false
+                    }}
                 >
                 </ReactAce>
             </div>
