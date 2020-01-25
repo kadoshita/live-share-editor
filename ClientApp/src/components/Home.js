5f61c1a73855634043a541269dc80f7f59d25c8c
kadoshita <sublimer.me@gmail.com>
2020-01-25 23:20:28 +0900

    add product advantage

--- ClientApp/src/components/Home.js
+++ ClientApp/src/components/Home.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import { Grid, Button, Paper } from '@material-ui/core';
+import { Grid, Button, Paper, Fade } from '@material-ui/core';
 import { Link } from 'react-router-dom';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faGithub, faTwitter } from '@fortawesome/free-brands-svg-icons';
@@ -7,6 +7,14 @@ import { faShareSquare, faCode, faTerminal } from '@fortawesome/free-solid-svg-i
 
 export class Home extends Component {
   static displayName = Home.name;
+  constructor(props) {
+    super(props);
+    this.state = {
+      isHoverItem1: false,
+      isHoverItem2: false,
+      isHoverItem3: false
+    };
+  }
 
   render() {
     return (
@@ -24,21 +32,38 @@ export class Home extends Component {
         <Grid item xs={12}>
           <Grid container spacing={2} style={{ textAlign: 'center' }}>
             <Grid item xs={4}>
-              <Paper elevation={4}>
+              <Paper elevation={4} onMouseOver={() => this.setState({ isHoverItem1: true })} onMouseLeave={() => this.setState({ isHoverItem1: false })}>
                 <h3>コーディングをリアルタイムで配信・共有</h3>
-                <FontAwesomeIcon icon={faShareSquare} color='#55B2B8' style={{ width: 'auto', height: '140px' }}></FontAwesomeIcon>
+                <div className='hover-item-parent'>
+                  <FontAwesomeIcon icon={faShareSquare} color='#55B2B8' className='hover-item-icon' fixedWidth size='9x'></FontAwesomeIcon>
+                  <Fade in={this.state.isHoverItem1} className='hover-item-fade'>
+                    <div><p>ASP.NET Core SignalRにより、低遅延でコーディングを配信・共有できます。</p></div>
+                  </Fade>
+                </div>
               </Paper>
             </Grid>
             <Grid item xs={4}>
-              <Paper elevation={4}>
+              <Paper elevation={4} onMouseOver={() => this.setState({ isHoverItem2: true })} onMouseLeave={() => this.setState({ isHoverItem2: false })}>
                 <h3>12言語・10カラーテーマに対応</h3>
-                <FontAwesomeIcon icon={faCode} color='#F8BBD0' style={{ width: 'auto', height: '140px' }}></FontAwesomeIcon>
+                <div className='hover-item-parent'>
+                  <FontAwesomeIcon icon={faCode} color='#F8BBD0' className='hover-item-icon' fixedWidth size='9x'></FontAwesomeIcon>
+                  <Fade in={this.state.isHoverItem2} className='hover-item-fade'>
+                    <div><p>C/C++、C#、Java、Python、PHP、Ruby、Go、JavaScript等、多数の言語のシンタックスハイライトが使用できます。</p></div>
+                  </Fade>
+                </div>
               </Paper>
             </Grid>
             <Grid item xs={4}>
-              <Paper elevation={4}>
+              <Paper elevation={4} onMouseOver={() => this.setState({ isHoverItem3: true })} onMouseLeave={() => this.setState({ isHoverItem3: false })}>
                 <h3>ブラウザ上で実行結果を確認</h3>
-                <FontAwesomeIcon icon={faTerminal} color='#55B2B8' style={{ width: 'auto', height: '140px' }}></FontAwesomeIcon>
+                <div className='hover-item-parent'>
+                  <FontAwesomeIcon icon={faTerminal} color='#55B2B8' className='hover-item-icon' fixedWidth size='9x'></FontAwesomeIcon>
+                  <Fade in={this.state.isHoverItem3} className='hover-item-fade'>
+                    <div>
+                      <p><a href='https://wandbox.org' target='_blank' rel='noopener noreferrer'>Wandbox</a>のWebAPIを使用して、書いたコードの実行結果をブラウザ上で確認できます。</p>
+                    </div>
+                  </Fade>
+                </div>
               </Paper>
             </Grid>
           </Grid>
