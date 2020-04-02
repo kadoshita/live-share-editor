ef031680949ad9735e46c08dde73376c6edcac98
kadoshita <sublimer.me@gmail.com>
2020-04-02 22:25:45 +0900

    add markdown preview

--- ClientApp/src/components/MarkdownPreviewDialog.js
+++ ClientApp/src/components/MarkdownPreviewDialog.js
@@ -1,13 +1,20 @@
 import React from 'react';
+import unified from 'unified';
+import parse from 'remark-parse';
+import remark2react from 'remark-react';
 import PropTypes from 'prop-types';
-import { TextField, Dialog, DialogActions, DialogContent, DialogTitle, Button } from '@material-ui/core';
+import { Dialog, DialogActions, DialogContent, DialogTitle, Button } from '@material-ui/core';
 
 const MarkdownPreviewDialog = props => {
     return (
         <Dialog open={props.show} fullWidth>
             <DialogTitle>Markdown Preview</DialogTitle>
             <DialogContent>
-                <TextField multiline fullWidth defaultValue={props.code}></TextField>
+                <div>
+                    {
+                        unified().use(parse).use(remark2react).processSync(props.code).result
+                    }
+                </div>
             </DialogContent>
             <DialogActions>
                 <Button onClick={() => props.togglOpen()}>OK</Button>
